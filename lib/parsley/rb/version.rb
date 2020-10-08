9506dd24890aed19dcdee98457e61be084e3ff02
Viral Patel <pviral810@gmail.com>
2020-10-08 17:27:57 -0400

    Run bundler gem

---
+++ lib/parsley/rb/version.rb
@@ -0,0 +1,5 @@
+module Parsley
+  module Rb
+    VERSION = "0.1.0"
+  end
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,4 @@
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "parsley/rb"
+
+require "minitest/autorun"
